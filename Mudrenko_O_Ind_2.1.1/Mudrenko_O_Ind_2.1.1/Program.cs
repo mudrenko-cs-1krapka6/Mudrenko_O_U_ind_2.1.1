4894daf28717dd7367b53cc40a2b21a8ae34c619
moipovilitel <cykablyatpizdamasonka@gmail.com>
2021-05-24 23:23:25 +0300

    Mudrenko_O_U_ind_2.1.1

---
+++ Mudrenko_O_Ind_2.1.1/Mudrenko_O_Ind_2.1.1/Program.cs
@@ -0,0 +1,46 @@
+﻿﻿/*
+ * Created by SharpDevelop.
+ * User: Ксюха
+ * Date: 19.04.2021
+ * Time: 19:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Mudrenko_O_Ind_2_1_2
+{
+	class Program
+	{
+		private static int [] array = new int [] {4, 5, 3, 1, 2};
+		private static int [] inArray () 
+		{
+			Console.WriteLine("Write array");
+			
+			return Array.ConvertAll (Console.ReadLine ().Split(), Convert.ToInt32);
+		}
+		public static int [] InsertionSort (int [] array)
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				int current = array[i];
+				int j = i;
+				while (j > 0 && current > array [j - 1])
+				{
+					array[j] = array [j - 1];
+					j--;
+				}
+				array [j] = current;
+			}
+			return array;
+		}
+		static void Main (string [] args)
+		{
+			foreach (var element in InsertionSort (inArray())) 
+			{
+				Console.Write (element + " ");
+			}
+			Console.ReadKey (true);
+		}
+	}
+}
